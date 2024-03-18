f6676fc9d31626cd16a33cd5b245fe83a90a3e70
ThalitaPeniche <162921132+ThalitaPeniche@users.noreply.github.com>
2024-03-18 17:34:11 -0300

    AULA ANTIGA

---
+++ programacao-11-03.py
@@ -0,0 +1,13 @@
+def e_vogal(letra):
+
+     if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+         return
+
+     return False
+
+print(e_vogal("e"))
+
+
+
+def media(a1, a2, as1)
+:
